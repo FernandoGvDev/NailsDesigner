ec71651da1da253cb13cfb0be5db8024f94c0e2c
AndBalbino <andersonbalbino2212@outlook.com>
2023-10-28 18:20:42 -0300

    Adicionando funcinalidades com Javascript e tornando o site responsivo

---
+++ main.js
@@ -0,0 +1,15 @@
+const now = new Date();
+const horas = now.getHours();
+let message = "Olá, gostaria de agendar um hoorário.";
+
+if (horas >= 6 && horas < 12){
+    message = "Bom dia, gostaria de agendar um horário.";
+} else if (horas >= 12 && horas < 18){
+    message = "Boa tarde, gostaria de agendar um horário.";
+} else if (horas >= 18){
+    message = "Boa noite, gostaria de agendar um horário.";
+}
+
+const whatsappLink = document.getElementById('whatsapp-link');
+whatsappLink.href = `https://api.whatsapp.com/send?phone=5511948379838&text=${encodeURIComponent(message)}`;
+
